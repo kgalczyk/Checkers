41bd8313a69a267ba8fc1fa798d062d7c427213b
Kacper <elanogaster@gmail.com>
2022-04-09 17:50:42 +0200

    poprawienie klas obiektów

--- static/js/Item.js
+++ static/js/Item.js
@@ -1,15 +1,24 @@
 class Field extends THREE.Mesh {
-    constructor(texture) {
-        super();
-        this.fieldWidth = 20; // Oś x
-        this.fieldDepth = 20; // Oś z
-        this.fieldHeight = 5; // Oś y
-        this.boxGeometry = new THREE.BoxGeometry(this.fieldWidth, this.fieldHeight, this.fieldDepth);
-        this.boxMaterial = new THREE.MeshBasicMaterial({
+    fieldWidth;
+    fieldHeight;
+    fieldDepth;
+
+    isPiece;
+    isField;
+
+    indexes;
+    constructor(texture, fieldWidth, fieldHeight, fieldDepth, x, y) {
+
+        const boxGeometry = new THREE.BoxGeometry(fieldWidth, fieldHeight, fieldDepth);
+        const boxMaterial = new THREE.MeshBasicMaterial({
             map: new THREE.TextureLoader().load(texture)
         });
-        this.box = new THREE.Mesh(this.boxGeometry, this.boxMaterial);
-        this.box.userData.isPiece = false; // Pole Obiekt3D.userData jest polem obiektu dla danych użytkownika
-        return this.box; // Konstruktor zwraca gotowy obiekt3D pola szachownicy
+        super(boxGeometry, boxMaterial);
+        this.fieldWidth = fieldWidth; // Oś x
+        this.fieldDepth = fieldDepth; // Oś z
+        this.fieldHeight = fieldHeight; // Oś y
+        this.isPiece = false; // Pole Obiekt3D.userData jest polem obiektu dla danych użytkownika
+        this.isField = true;
+        this.indexes = { x: x, y: y };
     }
 }
--- static/js/Piece.js
+++ static/js/Piece.js
@@ -1,43 +1,35 @@
 class Piece extends THREE.Mesh {
-    constructor(color, texture, pieceColor) {
-        super();
-        // wymiary cylindra
-        this.pieceRadiusTop = 7;
-        this.pieceRadiusBottom = 7;
-        this.pieceHeight = 5;
+    pieceRadiusTop;
+    pieceRadiusBottom;
+    pieceHeight;
+    segments;
+
+    positionInPiecesArray;
+    pieceColor;
+    isPiece;
+    constructor(color, texture, pieceColor, pieceRadiusTop, pieceRadiusBottom, pieceHeight, segments) {
         // geometria, materiał
-        this.cylinderGeometry = new THREE.CylinderGeometry(this.pieceRadiusTop, this.pieceRadiusBottom, this.pieceHeight, 25);
-        this.cylinderMaterial = new THREE.MeshBasicMaterial({
+        const cylinderGeometry = new THREE.CylinderGeometry(pieceRadiusTop, pieceRadiusBottom, pieceHeight, segments);
+        const cylinderMaterial = new THREE.MeshBasicMaterial({
             color: color,
             map: new THREE.TextureLoader().load(texture)
         })
-
         // stworzenie bierki
-        this.piece = new THREE.Mesh(this.cylinderGeometry, this.cylinderMaterial);
+        super(cylinderGeometry, cylinderMaterial);
+        // wymiary cylindra
+        this.pieceRadiusTop = pieceRadiusTop;
+        this.pieceRadiusBottom = pieceRadiusBottom;
+        this.pieceHeight = pieceHeight;
+        this.segments = segments;
 
         // dodane pola definiującego obiekt jako bierka
-        this.piece.userData.isPiece = true;
+        this.isPiece = true;
 
         // dodanie pola definiującego kolor bierki (1 lub 2);
-        this.piece.userData.pieceColor = pieceColor;
-
-        this.piece.userData.updatePositionInArray = (newPosition) => {
-            const WIDTH_STEP = gameManager.game.BOARD_WIDTH / gameManager.game.pieces.length;
-            const DEPTH_STEP = gameManager.game.BOARD_DEPTH / gameManager.game.pieces[0].length;
-
-            for (let i = -30, x = 0; i < 40; i += WIDTH_STEP, x++) {
-                for (let j = -30, y = 0; j < 40; j += DEPTH_STEP, y++) {
-                    if (newPosition.x === i && newPosition.z === j) {
-                        this.piece.userData.positionInPiecesArray = {
-                            x: x,
-                            y: y
-                        }
-                        return { x: x, y: y };
-                    }
-                }
-            }
-            return null;
-        }
-        return this.piece;
+        this.pieceColor = pieceColor;
     }
+
+    updatePositionInArray = (newPosition) => {
+        this.positionInPiecesArray = { x: newPosition.x, y: newPosition.y };
+    };
 }
