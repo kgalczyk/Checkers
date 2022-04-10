d4cf7dbc3d47158b54a08adc2080ce9c02c0c4f2
Kacper <elanogaster@gmail.com>
2022-04-10 22:37:10 +0200

    działanie naprzemienności ruchu graczy

--- static/js/Raycaster.js
+++ static/js/Raycaster.js
@@ -3,14 +3,33 @@ class Raycaster {
     mouseVector;
     mouseMove;
     intersects;
-    piece;
 
-    constructor() {
+    piece; // pole na obecnie podniesioną bierkę
+    pieces; // tablica obiektów bierek rodem z Game'a
+    fields; // tablica obiektów pól rodem z Game'a
+
+    // kolory figur
+    heldPiecesColor; // żółty bądź czerwony w hexie
+    originalPieceColor; // biały bądź czarny w hexie
+
+    // kolor pola (1 - biały lub 2 - czarny)
+    piecesColor;
+    whoseTurn = true;
+    color;
+
+    constructor(pieces, fields, piecesColor) {
         this.raycaster = new THREE.Raycaster();
         this.mouseVector = new THREE.Vector2();
         this.mouseMove = new THREE.Vector2();
         this.intersects = [];
         this.piece = null;
+        this.pieces = pieces;
+        this.fields = fields;
+        this.heldPiecesColor = piecesColor === 1 ? 0xffff00 : 0xff00ff;
+        this.originalPieceColor = piecesColor === 1 ? 0xffffff : 0x4455aa;
+        this.color = piecesColor === 1 ? true : false;
+
+        this.piecesColor = piecesColor;
         console.log("stworzono Raycaster");
         window.onclick = this.clickOnBoard;
         window.onmousemove = this.moveMouse;
@@ -28,6 +47,10 @@ class Raycaster {
 
         if (this.piece) {
             if (intersects.length > 0 && intersects[0].object.isField) {
+                console.log(this.whoseTurn, this.color);
+                if (this.piece.pieceColor !== this.piecesColor) return;
+                if (this.whoseTurn !== this.color) return;
+
                 let field = this.findSquareForThePiece(intersects[0].point);
                 if (field === undefined) return;
 
@@ -36,25 +59,26 @@ class Raycaster {
                 let newPiecePositionInArray = field.indexes;// Przekazanie nowej pozycji do konwersji na współrzędne w tablicy bierek
                 this.piece.updatePositionInArray(newPiecePositionInArray);
 
+                // Wysłanie do serwera nowej pozycji
+                gameManager.net.sendNewPosition(oldPiecePositionInArray, newPiecePositionInArray, this.piece.position, field.position);
+
                 // wykonanie ruchu -> TWEEN
                 const move = new MovementAnimation(this.piece, field.position.x, field.position.z);
                 move.startMove();
-
-                // Wysłanie do serwera nowej pozycji
-                gameManager.net.sendNewPosition(oldPiecePositionInArray, newPiecePositionInArray);
             }
-            if (this.piece.pieceColor === 1) this.piece.material.color.set(0xffffff);
-            else this.piece.material.color.set(0x4455aa);
+
+            this.piece.material.color.set(this.originalPieceColor);
+
             this.piece = null;
         }
 
         if (intersects.length > 0 && intersects[0].object.isPiece) {
             this.piece = intersects[0].object;
-            console.log(this.piece);
-
-            if (this.piece.pieceColor === 1) this.piece.material.color.set(0xffff00);
-            else this.piece.material.color.set(0xff00ff);
-
+            if (this.piece.pieceColor !== this.piecesColor) {
+                this.piece = null;
+                return;
+            }
+            this.piece.material.color.set(this.heldPiecesColor);
             return;
         }
     }
@@ -65,7 +89,7 @@ class Raycaster {
     }
 
     findSquareForThePiece = (point) => {
-        return gameManager.game.fieldsObjects.find((field) => {
+        return this.fields.find((field) => {
             // Wyznaczenie odległosci miejsca kliknięcia od pozycji pola
             const DISTANCE_FROM_MOUSE_X_TO_THE_MIDDLE_POINT = Math.abs(point.x - field.position.x);
             const DISTANCE_FROM_MOUSE_Y_TO_THE_MIDDLE_POINT = Math.abs(point.z - field.position.z);
